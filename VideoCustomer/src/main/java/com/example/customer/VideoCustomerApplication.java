eebae490b9a5753fd64e0e25cf16faa4854cdc24
Mansi411 <52767573+Mansi411@users.noreply.github.com>
2022-09-23 16:30:48 +0530

    Add files via upload

---
+++ VideoCustomer/src/main/java/com/example/customer/VideoCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.example.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoCustomerApplication.class, args);
+	}
+
+}
